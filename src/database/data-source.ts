e1a229618b73942f92a8951034b03a8fe63b47b8
JoseMontesU <josemontes.0@hotmail.com>
2023-07-26 16:52:29 -0500

    migraciones y dotenv

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm";
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: 'postgres://root:neo.123@192.168.233.129:5432/mydb',
+  url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
   entities: ['src/**/*.entity.ts'],
-  // entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['src/database/migrations/*.ts'],
   migrationsTableName: 'migrations',
 })
